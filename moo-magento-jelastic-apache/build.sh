f4264e59cfb4559b14812c65d794271c53c9061f
Ahmed Rizawan <ahm.rizawan@gmail.com>
2022-08-17 18:27:24 +0600

    Updated

--- moo-magento-jelastic-apache/build.sh
+++ moo-magento-jelastic-apache/build.sh
@@ -1,3 +1,5 @@
 IMAGENAME=arizawan/magento-apache-php
 IMAGETAG=7.4-apache
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t $IMAGENAME:$IMAGETAG --push .
+#docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t $IMAGENAME:$IMAGETAG --push .
+docker build -t $IMAGENAME:$IMAGETAG .
+docker push $IMAGENAME:$IMAGETAG
